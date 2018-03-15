800a60789a733b6fb5e0260539572fca1c1509a3
Alejandro Krumkamp <alekd9@gmail.com>
2018-03-15 01:02:50 -0300

    Debugging fetch from Medium Lambda feed

--- src/components/FeedContainer/FeedContainer.js
+++ src/components/FeedContainer/FeedContainer.js
@@ -1,17 +1,37 @@
-import React from 'react';
+import React, {Component} from 'react';
+import fetch from 'isomorphic-fetch';
 
-const FeedContainer = (props) => {
-    return (
-        <div id="FeedContainer">
-            feed
-            {/*language=CSS*/}
-            <style jsx>{`
-                #FeedContainer {
-                    background:red;
-                }
-        `}</style>
-        </div>
-    );
+export default class FeedContainer extends Component{
+
+    componentDidMount(){
+        this.fetchFeed();
+    }
+
+    async fetchFeed() {
+        let req = await fetch('https://umr8fj85l5.execute-api.us-east-1.amazonaws.com/prod/medium-alekrumkamp-feed');
+        let json = await req.json();
+        let articles = await json.payload.references.Post;
+        console.log(articles);
+
+        Object.keys(articles)
+            .forEach((key,index) => {
+                console.log(key)
+                console.log(articles.key.title)
+        })
+        return articles;
+    }
+
+    render() {
+        return (
+            <div id="FeedContainer">
+
+                {/*language=CSS*/}
+                <style jsx>{`
+                    #FeedContainer {
+                        background: red;
+                    }
+                `}</style>
+            </div>
+        );
+    }
 }
-
-export default FeedContainer;
